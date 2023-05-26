b8f07c8420f9cd980d30b35853865da1aa89c3e7
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-05-25 18:31:17 -0700

    first commit

---
+++ src/main/java/com/asn1/htmlcss/HtmlcssApplication.java
@@ -0,0 +1,13 @@
+package com.asn1.htmlcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlcssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlcssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asn1/htmlcss/HtmlcssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asn1.htmlcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlcssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
