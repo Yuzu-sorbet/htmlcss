7cda6bc4033ab127f81779cb0ebdec28031d268d
Yuzu-sorbet <126542378+Yuzu-sorbet@users.noreply.github.com>
2023-05-30 16:55:54 -0700

    all lower bounds added

--- src/main/resources/static/js/calculate.js
+++ src/main/resources/static/js/calculate.js
@@ -30,7 +30,6 @@ var d1 = document.getElementById('d1');
 var f1 = document.getElementById('f1');
 
 var i = 0;
-var count = 0;
 
 function hist(grading){
 //add number of students to each grade - accurate to histogram
@@ -38,48 +37,36 @@ function hist(grading){
     for (; i < length; i++){
         if (grading[i] >= aplus){
             a1.textContent += '0';
-            console.log('activated a+');
         } 
         else if(grading[i] >= a){
             a2.textContent +='0';
-            console.log('activated a');
         }
         else if(grading[i] >= aminus){
             a3.textContent +='0';
-            console.log('activated a-');
         }
         else if(grades[i] >= bplus){
             b1.textContent +='0';
-            count ++;
-            console.log('activated b+');
         }
         else if(grading[i] >= b){
             b2.textContent +='0';
-            console.log('activated b');
         }
         else if(grading[i] >= bminus){
             b3.textContent +='0';
-            console.log('activated b-');
         }
         else if(grading[i] >= cplus){
             c1.textContent +='0';
-            console.log('activated c+');
         }
         else if(grading[i] >= c){
             c2.textContent +='0';
-            console.log('activated c');
         }
         else if(grading[i] >= cminus){
             c3.textContent +='0';
-            console.log('activated c-');
         }
         else if(grading[i] >= d){
             d1.textContent +='0';
-            console.log('activated d');
         }
-        else{ //50
+        else{ 
             f1.textContent +='0';
-            console.log('activated f');
         }
     }
     return
@@ -100,12 +87,10 @@ button.addEventListener('click', function(evt){
         inputbox.style.border = "2px solid red";
         error.textContent = "Invalid input. Please enter integer/float values.";
     }
-
     else{
         if (isValid(newgrade)){
             if (parseFloat(newgrade)){
                 //update histogram
-                console.log(newgrade);
                 grades.push(newgrade);
                 hist(grades);
                 inputbox.style.border = "1px solid black";
@@ -128,6 +113,93 @@ function isValid(grade){
     return false
 }
 
+//readjust grade bounds A+
+var gradeaplus = document.getElementById('a+');
+gradeaplus.addEventListener("keypress", gradea1);
+
+//function for adjusting A+ bounds 
+function gradea1(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradea1 = document.getElementById('a+').value;
+      var lowerbound = document.getElementById('a').value;
+      var upperbound = document.getElementById('max').value;
+      if (isNaN(gradea1)){
+          document.getElementById('a+').style.border = "2px solid red";
+          error2.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradea1)){
+              if (lowerbound < gradea1 && gradea1 < upperbound){
+                  parseFloat(gradea1);
+                  //set new grade bound
+                  aplus = gradea1;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('a+').style.border = "1px solid black";
+                  error2.textContent = "";
+              }
+              else{
+                  document.getElementById('a+').style.border = "2px solid red";
+                  error2.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('a+').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+//readjust grade bounds A
+var gradea = document.getElementById('a');
+gradea.addEventListener("keypress", gradea2);
+
+//function for adjusting A bounds 
+function gradea2(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradea2 = document.getElementById('a').value;
+      var lowerbound = document.getElementById('a-').value;
+      var upperbound = document.getElementById('a+').value;
+
+    //update to next error div box
+      var error3 = document.getElementsByClassName('error2')[1];
+      if (isNaN(gradea2)){
+          document.getElementById('a').style.border = "2px solid red";
+          error3.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradea2)){
+              if (lowerbound < gradea2 && gradea2 < upperbound){
+                  parseFloat(gradea2);
+                  //set new grade bound
+                  aplus = gradea2;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('a').style.border = "1px solid black";
+                  error3.textContent = "";
+              }
+              else{
+                  document.getElementById('a').style.border = "2px solid red";
+                  error3.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('a').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
 //readjust grade bounds A-
 var gradeaminus = document.getElementById('a-');
 gradeaminus.addEventListener("keypress", gradea3);
@@ -139,28 +211,30 @@ function gradea3(evt){
       // Cancel the default action
       evt.preventDefault();
       //check for valid grade adjustment
-      var gradea = document.getElementById('a-').value;
+      var gradea3 = document.getElementById('a-').value;
       var lowerbound = document.getElementById('b+').value;
       var upperbound = document.getElementById('a').value;
-      if (isNaN(gradea)){
+
+      //update to next error div box
+      var error4 = document.getElementsByClassName('error2')[2];
+      if (isNaN(gradea3)){
           document.getElementById('a-').style.border = "2px solid red";
-          error2.textContent = "Invalid input.";
+          error4.textContent = "Invalid input.";
       }
       else{
-          if (isValid(gradea)){
-              if (lowerbound < gradea && gradea < upperbound){
-                  parseFloat(gradea);
-                  console.log(gradea);
+          if (isValid(gradea3)){
+              if (lowerbound < gradea3 && gradea3 < upperbound){
+                  parseFloat(gradea3);
                   //set new grade bound
-                  aminus = gradea;
+                  aminus = gradea3;
                   //update histogram
                   hist(grades);
                   document.getElementById('a-').style.border = "1px solid black";
-                  error2.textContent = "";
+                  error4.textContent = "";
               }
               else{
                   document.getElementById('a-').style.border = "2px solid red";
-                  error2.textContent = "Invalid input.";
+                  error4.textContent = "Invalid input.";
               }
           }
           else{
@@ -173,5 +247,308 @@ function gradea3(evt){
 
 //readjust grade bounds B+
 var gradebplus = document.getElementById('b+');
-gradeaminus.addEventListener("keypress", gradea3);
+gradebplus.addEventListener("keypress", gradeb1);
 
+//function for adjusting B+ bounds 
+function gradeb1(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradeb1 = document.getElementById('b+').value;
+      var lowerbound = document.getElementById('b').value;
+      var upperbound = document.getElementById('a-').value;
+      //update to next error div box
+      var error5 = document.getElementsByClassName('error2')[3];
+      if (isNaN(gradeb1)){
+          document.getElementById('b+').style.border = "2px solid red";
+          error5.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradeb1)){
+              if (lowerbound < gradeb1 && gradeb1 < upperbound){
+                  parseFloat(gradeb1);
+                  //set new grade bound
+                  bplus = gradeb1;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('b+').style.border = "1px solid black";
+                  error5.textContent = "";
+              }
+              else{
+                  document.getElementById('b+').style.border = "2px solid red";
+                  error5.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('b+').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+  //readjust grade bounds B
+var gradeb = document.getElementById('b');
+gradeb.addEventListener("keypress", gradeb2);
+
+//function for adjusting B bounds 
+function gradeb2(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradeb2 = document.getElementById('b').value;
+      var lowerbound = document.getElementById('b-').value;
+      var upperbound = document.getElementById('b+').value;
+      //update to next error div box
+      var error6 = document.getElementsByClassName('error2')[4];
+      if (isNaN(gradeb2)){
+          document.getElementById('b').style.border = "2px solid red";
+          error6.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradeb2)){
+              if (lowerbound < gradeb2 && gradeb2 < upperbound){
+                  parseFloat(gradeb2);
+                  //set new grade bound
+                  b = gradeb2;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('b').style.border = "1px solid black";
+                  error6.textContent = "";
+              }
+              else{
+                  document.getElementById('b').style.border = "2px solid red";
+                  error6.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('b').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+//readjust grade bounds B-
+var gradebminus = document.getElementById('b-');
+gradebminus.addEventListener("keypress", gradeb3);
+
+//function for adjusting B- bounds 
+function gradeb3(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradeb3 = document.getElementById('b-').value;
+      var lowerbound = document.getElementById('c+').value;
+      var upperbound = document.getElementById('b').value;
+      //update to next error div box
+      var error7 = document.getElementsByClassName('error2')[5];
+      if (isNaN(gradeb3)){
+          document.getElementById('b-').style.border = "2px solid red";
+          error7.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradeb3)){
+              if (lowerbound < gradeb3 && gradeb3 < upperbound){
+                  parseFloat(gradeb3);
+                  //set new grade bound
+                  bminus = gradeb3;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('b-').style.border = "1px solid black";
+                  error7.textContent = "";
+              }
+              else{
+                  document.getElementById('b-').style.border = "2px solid red";
+                  error7.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('b-').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+//readjust grade bounds C+
+var gradecplus = document.getElementById('c+');
+gradecplus.addEventListener("keypress", gradec1);
+
+//function for adjusting C+ bounds 
+function gradec1(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradec1 = document.getElementById('c+').value;
+      var lowerbound = document.getElementById('c').value;
+      var upperbound = document.getElementById('b-').value;
+      //update to next error div box
+      var error8 = document.getElementsByClassName('error2')[6];
+      if (isNaN(gradec1)){
+          document.getElementById('c+').style.border = "2px solid red";
+          error8.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradec1)){
+              if (lowerbound < gradec1 && gradec1 < upperbound){
+                  parseFloat(gradec1);
+                  //set new grade bound
+                  cplus = gradec1;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('c+').style.border = "1px solid black";
+                  error8.textContent = "";
+              }
+              else{
+                  document.getElementById('c+').style.border = "2px solid red";
+                  error8.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('c+').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+//readjust grade bounds C
+var gradec = document.getElementById('c');
+gradec.addEventListener("keypress", gradec2);
+
+//function for adjusting C bounds 
+function gradec2(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradec2 = document.getElementById('c').value;
+      var lowerbound = document.getElementById('c-').value;
+      var upperbound = document.getElementById('c+').value;
+      //update to next error div box
+      var error9 = document.getElementsByClassName('error2')[7];
+      if (isNaN(gradec2)){
+          document.getElementById('c').style.border = "2px solid red";
+          error9.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradec2)){
+              if (lowerbound < gradec2 && gradec2 < upperbound){
+                  parseFloat(gradec2);
+                  //set new grade bound
+                  c = gradec2;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('c').style.border = "1px solid black";
+                  error9.textContent = "";
+              }
+              else{
+                  document.getElementById('c').style.border = "2px solid red";
+                  error9.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('c').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+  //readjust grade bounds C-
+var gradecminus = document.getElementById('c-');
+gradecminus.addEventListener("keypress", gradec3);
+
+//function for adjusting C- bounds 
+function gradec3(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var gradec3 = document.getElementById('c-').value;
+      var lowerbound = document.getElementById('d').value;
+      var upperbound = document.getElementById('c').value;
+      //update to next error div box
+      var error10 = document.getElementsByClassName('error2')[8];
+      if (isNaN(gradec3)){
+          document.getElementById('c-').style.border = "2px solid red";
+          error10.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(gradec3)){
+              if (lowerbound < gradec3 && gradec3 < upperbound){
+                  parseFloat(gradec3);
+                  //set new grade bound
+                  cminus = gradec3;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('c-').style.border = "1px solid black";
+                  error10.textContent = "";
+              }
+              else{
+                  document.getElementById('c-').style.border = "2px solid red";
+                  error10.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('c-').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
+
+//readjust grade bounds D
+var graded = document.getElementById('d');
+graded.addEventListener("keypress", graded1);
+
+//function for adjusting D bounds 
+function graded1(evt){
+    // check when enter is pressed
+    if (evt.key === "Enter") {
+      // Cancel the default action
+      evt.preventDefault();
+      //check for valid grade adjustment
+      var graded1 = document.getElementById('d').value;
+      var lowerbound = document.getElementById('f').value;
+      var upperbound = document.getElementById('c-').value;
+      //update to next error div box
+      var error11 = document.getElementsByClassName('error2')[9];
+      if (isNaN(graded1)){
+          document.getElementById('d').style.border = "2px solid red";
+          error11.textContent = "Invalid input.";
+      }
+      else{
+          if (isValid(graded1)){
+              if (lowerbound < graded1 && graded1 < upperbound){
+                  parseFloat(graded1);
+                  //set new grade bound
+                  d = graded1;
+                  //update histogram
+                  hist(grades);
+                  document.getElementById('d').style.border = "1px solid black";
+                  error11.textContent = "";
+              }
+              else{
+                  document.getElementById('d').style.border = "2px solid red";
+                  error11.textContent = "Invalid input.";
+              }
+          }
+          else{
+              document.getElementById('d').style.border = "2px solid red";
+  
+          }
+      }
+    }
+  }
